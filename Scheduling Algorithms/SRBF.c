9b1973faa6cb6996d7912efd4e3db22542ca6436
George Rahul <75750164+georgerahul24@users.noreply.github.com>
2024-02-03 00:05:10 +0530

    SRBF Also Completed
    TODO: Maybe the scheduling algo has to be optimised to check when the next process comes instead of just increasing the time

--- Scheduling Algorithms/SRBF.c
+++ Scheduling Algorithms/SRBF.c
@@ -1,9 +1,43 @@
 #include "process.h"
 
 
+Process *findMinNonCompleted(ProcessArray *pa, int currentTime) {
+    int minBurstTime = INT32_MAX;
+    Process *minProcess = NULL;
+    for (int i = 0; i < pa->numberOfProcesses; i++) {
+        Process *temp = pa->processes[i];
+        int remainingBurstTime = temp->cpu_burst - temp->time_received;
+
+        if (remainingBurstTime < minBurstTime && temp->finished == 0 && temp->arrival <= currentTime) {
+            minProcess = temp;
+            minBurstTime=remainingBurstTime;
+        }
+
+    }
+    return minProcess;
+
+}
+
+
 int main() {
     int currentTime = 0, completed_process = 0;
-    ProcessArray *pa = readProcessesFromFile("processes.txt");
+    ProcessArray *pa = readProcessesFromFile("/Users/georgerahul/Desktop/OS-Lab/Scheduling Algorithms/processes.txt");
     printProcessTable(pa);
+
+    while (completed_process < pa->numberOfProcesses) {
+        Process *p = findMinNonCompleted(pa, currentTime);
+        if (p != NULL) {
+            p->time_received += 1;
+            printf("%s is executing at time %d\n", p->name, currentTime);
+            if ((p->cpu_burst - p->time_received) == 0) {
+                p->finished = 1;
+                p->timeCompleted = currentTime + 1;
+                completed_process+=1;
+            }
+        }
+        currentTime += 1;
+    }
+    return 0;
+
 
 }
